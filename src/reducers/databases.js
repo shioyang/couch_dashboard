cfb0bbe2e71c787efb54735dc80ee07173af3096
shioyang <shioyang4321@gmail.com>
2017-02-23 16:56:49 +0900

    Fix reducer for databases

--- src/reducers/databases.js
+++ src/reducers/databases.js
@@ -3,9 +3,6 @@ import { SHOW_DOCUMENT_LIST, showDocumentListAction } from '../actions'
 const databases = (state = [{name: "host"}], action) => {
   switch(action.type){
     case SHOW_DOCUMENT_LIST:
-      return {
-        name: action.name
-      }
     default:
       return state
   }
