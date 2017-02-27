9c89c9d69901652611de347a9b7795151245dc05
shioyang <shioyang4321@gmail.com>
2017-02-27 15:53:47 +0900

    Add title and style

--- src/presentations/App.js
+++ src/presentations/App.js
@@ -3,7 +3,8 @@ import SelectDatabase from '../containers/SelectDatabase'
 import './App.css'
 
 const App = () => (
-  <div>
+  <div className='couchDashboardApp'>
+    <h1>Couch Dashboard</h1>
     <SelectDatabase />
   </div>
 )
